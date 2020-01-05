776cfc73fe3d7a064de25bdfd8d922b1861834ad
hgugic@gmail.com <hgugic@gmail.com>
2020-01-05 17:26:56 +0100

    End of Section 5

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -55,7 +59,22 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler(builder => {
+                    builder.Run(async httpContext => {
+                        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
+                        var error = httpContext.Features.Get<IExceptionHandlerFeature>();
+                        if(error != null)
+                        {
+                            httpContext.Response.AddApplicationError(error.Error.Message);
+                            await httpContext.Response.WriteAsync(error.Error.Message); 
+                        }
+
+                    });
+                });
+            }
             //app.UseHttpsRedirection();
 
             app.UseRouting();
